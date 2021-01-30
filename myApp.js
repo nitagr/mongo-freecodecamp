e39dd912802a9bb52ac3a525f3461cf7232bf5a0
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-01-30 11:22:42 +0530

    feat: db connection setup

--- myApp.js
+++ myApp.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGO_URI, connectionParams)
+  .then(() => {
+    console.log('Connected to database ');
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
 
 let Person;
 
